d2bce05b372713c85b7ae9e0b9b685be1c1a0fee
何吓吓 <hmhlonely@163.com>
2023-03-10 00:13:49 +0800

    表格暂存

--- cmd/base/FileUtils.ts
+++ cmd/base/FileUtils.ts
@@ -29,21 +29,4 @@ export class FileUitls {
         return [];
     }
 
-    /**
-     * 复制文件或目录。目录可以包含内容。
-     * 
-     * @param src 源文件路径  注意，如果src是一个目录，它将复制该目录中的所有内容，而不是整个目录本身。
-     * @param dest 目标文件路径  请注意，如果src是文件，则dest不能是目录。
-     * @param opt 参数
-     *  overwrite ＜boolean＞：覆盖现有文件或目录，默认值为true。请注意，如果将此设置为false并且目标存在，则复制操作将自动失败。使用errorOnExist选项更改此行为。
-     *  errorOnExist＜boolean＞：当overwrite为false且目标存在时，抛出错误。默认值为false。
-     *  dereference＜boolean＞：解引用符号链接，默认值为false。dereference symlinks, default is false
-     *  preserveTimestamps＜boolean＞：如果为true，将设置对原始源文件的最后修改和访问时间。如果为false，则时间戳行为取决于操作系统。默认值为false。
-     *  filter＜Function＞：过滤复制的文件/目录。返回true复制，返回false忽略。
-     */
-    static copyFile(src: string, dest: string, opt?: _fse.CopyOptionsSync) {
-        _fse.copySync(src, dest, opt);
-    }
-
-
 }
--- cmd/src/cmd_xlsx.ts
+++ cmd/src/cmd_xlsx.ts
@@ -1,4 +1,6 @@
 import _path from "path";
+import _xlsx from "xlsx";
+import _fse from "fs-extra";
 import { FileUitls } from "../base/FileUtils";
 import { logger } from "../base/Logger";
 
@@ -10,19 +12,52 @@ export class CmdXlsx {
     /** 执行函数 */
     exec() {
         let itself = this;
-        let dir = _path.join(__dirname, "../", "xlsx");
+        let bastPath = _path.join(__dirname, "../");
+        let dir = _path.join(bastPath, "xlsx");
         let dirlist = FileUitls.getDirList(dir);
         logger.info(dirlist)
+        for (let index = 0, len = dirlist.length; index < len; index++) {
+            let name = dirlist[index];
+            let suffix = _path.extname(name);
+            if (suffix != ".xlsx") {
+                logger.info("不是：", name)
+                continue;
+            }
+            if (!name.match(/^[a-zA-Z0-9]+/)) {
+                logger.info("不是：", name)
+                continue;
+            }
+            let filePath = _path.join(dir, dirlist[index]);
+            itself.readXlsx(filePath);
+        }
+        //写入
 
     }
 
     /**
      * 读xlsx文件
-     * @param file 
+     * @param src 
      */
-    readXlsx(file: string) {
+    readXlsx(src: string) {
         let itself = this;
+        let data = _xlsx.readFile(src);
+        let sheetNames = data.SheetNames || [];
+        for (let index = 0, len = sheetNames.length; index < len; index++) {
+            let sheetJson = data.Sheets[sheetNames[index]];
 
+        }
+        let xlsxName = itself.getXlsxName(src);
+        _fse.outputJsonSync(_path.join(__dirname, "../", "xlsx", xlsxName + ".json"), data);
+    }
+    /**
+     * 获取xlsx文件名
+     * @param src 
+     */
+    private getXlsxName(src: string) {
+        let xlsxName = _path.basename(src);
+        let matchName = xlsxName.match(/[a-zA-Z0-9]+/) || [];
+        let name = matchName[0] || "";
+        return name;
     }
 }
 new CmdXlsx().exec();
