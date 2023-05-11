0bc72b2e51ea123f21e551ee1a4f9585e609610c
huoevl <huoevl@163.com>
2023-05-11 20:47:29 +0800

    删除

--- assets/scripts/base/comp/ScrollContent.ts
+++
@@ -1,6 +0,0 @@
-import { Component, _decorator } from "cc";
-const { ccclass, property, executeInEditMode } = _decorator;
-@ccclass('ScrollPane')
-export class ScrollPane extends Component {
-
-}
--- 笔记/收集/anything-about-game-干货分享群.md
+++
@@ -1,3 +0,0 @@
-### anything-about-game-干货分享群（1067123079）
-1. [终于找到 舒服的 shader变体统计工具了](https://github.com/Unity-Technologies/UnityDataTools)  
-2. 
